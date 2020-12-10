8d21d73226f3291422b5281cc5049d13f90f5df4
may arden <mayard3n@gmail.com>
2020-12-10 14:45:52 -0800

    commit tree bc6fb96649f962f047b22a71d9104b023a15dd93

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,18 @@
 class Helpers
+
+    def self.current_user(session)
+        @user = User.find_by_id(session[:user_id])
+    end 
+        # should accept the session hash as an argument
+        # use user_id from the session hash to find the user in the database
+        # and return that user 
+        
+
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+        # should also accept the session hash as an argument
+        # this method will return true if the user_id
+        # is in the session hash and false if not
+    end 
+
 end
